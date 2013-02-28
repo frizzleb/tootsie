c49e7322c190e04f61b7356e31085fe0203cfa8f
Alexander Staubo <alex@bengler.no>
2013-02-28 18:48:48 +0100

    Add more tests.

--- spec/helpers/image_helper.rb
+++ spec/helpers/image_helper.rb
@@ -1,6 +1,6 @@
 module ImageHelper
 
-  def process_image_versions(test_file_name, version)
+  def process_image_version(test_file_name, version)
     result, content = nil, nil
     Tempfile.open("specs", :encoding => 'binary') do |file|
       file.close
@@ -10,13 +10,15 @@ module ImageHelper
       proc = ImageProcessor.new(
         :input_url => test_file_url(test_file_name),
         :versions => [version])
-      proc.valid?.should == true
+      proc.valid?.should eq true
+
       result = proc.execute!
       result.should have_key(:outputs)
-      result[:outputs].length.should == 1
+      result[:outputs].length.should eq 1
       result[:outputs][0].should have_key(:url)
 
       content = File.read(file.path)
+      content.length.should be > 0
     end
     [result, content]
   end
@@ -25,10 +27,13 @@ module ImageHelper
     Tempfile.open("specs", :encoding => 'binary') do |file|
       file << image_data
       file.close
-      IO.popen("identify -format '%w %h' '#{file.path}'", 'r') do |f|
+      IO.popen("identify -format '%w %h %[EXIF:Orientation]' '#{file.path}'", 'r') do |f|
         f.each_line do |line|
-          if line =~ /(\d+) (\d+)/
-            width, height = $1.to_i, $2.to_i
+          if line =~ /(\d+) (\d+) (\d*)/
+            width, height, orientation = $1.to_i, $2.to_i, $3.to_i
+            if [5, 6, 7, 8].include?(orientation)
+              width, height = height, width
+            end
             return width, height
           end
         end
--- spec/tootsie/image_processor_spec.rb
+++ spec/tootsie/image_processor_spec.rb
@@ -8,55 +8,54 @@ describe ImageProcessor do
 
   describe 'metadata' do
     it 'returns basic metadata from the original image' do
-      result, contents = process_image_versions('landscape.jpeg',
-        {:width => 50, :height => 50})
-      result[:width].should == 360
-      result[:height].should == 240
-      result[:depth].should == 8
+      result, contents = process_image_version('landscape.jpeg', {})
+      result[:width].should eq 360
+      result[:height].should eq 240
+      result[:depth].should eq 8
     end
 
-    %w(
-      Exif.Image.ImageDescription
-      Exif.Image.Orientation
-      Exif.Image.XResolution
-      Exif.Image.YResolution
-      Exif.Image.ResolutionUnit
-      Exif.Image.Software
-      Exif.Image.DateTime
-      Exif.Image.Artist
-      Exif.Image.Copyright
-      Exif.Image.ExifTag
-      Exif.Photo.ColorSpace
-      Exif.Photo.PixelXDimension
-      Exif.Photo.PixelYDimension
-      Exif.Thumbnail.Compression
-      Exif.Thumbnail.XResolution
-      Exif.Thumbnail.YResolution
-      Exif.Thumbnail.ResolutionUnit
+    it "returns EXIF and IPTC metadata from the original image that has such metadata" do
+      result, contents = process_image_version('iptc_xmp.jpeg', {})
+      metadata = result[:metadata]
+      metadata.should be_a_kind_of(Hash)
 
-      Iptc.Application2.RecordVersion
-      Iptc.Application2.Caption
-      Iptc.Application2.Writer
-      Iptc.Application2.Headline
-      Iptc.Application2.SpecialInstructions
-      Iptc.Application2.Byline
-      Iptc.Application2.BylineTitle
-      Iptc.Application2.Credit
-      Iptc.Application2.Source
-      Iptc.Application2.ObjectName
-      Iptc.Application2.DateCreated
-      Iptc.Application2.City
-      Iptc.Application2.ProvinceState
-      Iptc.Application2.CountryName
-      Iptc.Application2.TransmissionReference
-      Iptc.Application2.Keywords
-      Iptc.Application2.Copyright
-    ).each do |key|
-      it "returns metadata property '#{key}' from the original image" do
-        result, contents = process_image_versions('iptc_xmp.jpeg',
-          {:width => 50, :height => 50})
-        metadata = result[:metadata]
-        metadata.should be_a_kind_of(Hash)
+      %w(
+        Exif.Image.ImageDescription
+        Exif.Image.Orientation
+        Exif.Image.XResolution
+        Exif.Image.YResolution
+        Exif.Image.ResolutionUnit
+        Exif.Image.Software
+        Exif.Image.DateTime
+        Exif.Image.Artist
+        Exif.Image.Copyright
+        Exif.Image.ExifTag
+        Exif.Photo.ColorSpace
+        Exif.Photo.PixelXDimension
+        Exif.Photo.PixelYDimension
+        Exif.Thumbnail.Compression
+        Exif.Thumbnail.XResolution
+        Exif.Thumbnail.YResolution
+        Exif.Thumbnail.ResolutionUnit
+
+        Iptc.Application2.RecordVersion
+        Iptc.Application2.Caption
+        Iptc.Application2.Writer
+        Iptc.Application2.Headline
+        Iptc.Application2.SpecialInstructions
+        Iptc.Application2.Byline
+        Iptc.Application2.BylineTitle
+        Iptc.Application2.Credit
+        Iptc.Application2.Source
+        Iptc.Application2.ObjectName
+        Iptc.Application2.DateCreated
+        Iptc.Application2.City
+        Iptc.Application2.ProvinceState
+        Iptc.Application2.CountryName
+        Iptc.Application2.TransmissionReference
+        Iptc.Application2.Keywords
+        Iptc.Application2.Copyright
+      ).each do |key|
         metadata.should have_key(key)
         if metadata[key].is_a?(Array)
           metadata[key].each do |value|
@@ -79,43 +78,137 @@ describe ImageProcessor do
     end
   end
 
-  describe 'resizing' do
-    it 'resizes image, preserving aspect ratio (landscape)' do
-      result, contents = process_image_versions('landscape.jpeg',
-        {:width => 50, :height => 50})
-
-      result[:outputs].length.should == 1
-
-      contents.length.should > 0
-      width, height = extract_dimensions(contents)
-      width.should == 50
-      height.should == 33
-    end
-
-    it 'resizes image, preserving aspect ratio (portrait)' do
-      result, contents = process_image_versions('portrait.jpeg',
-        {:width => 50, :height => 50})
-
-      result[:outputs].length.should == 1
-
-      contents.length.should > 0
-      width, height = extract_dimensions(contents)
-      width.should == 33
-      height.should == 50
+  describe 'format conversion' do
+    %w(jpeg tiff gif png).each do |format|
+      it "converts images to #{format}" do
+        result, contents = process_image_version('landscape.jpeg', {:format => format})
+        result[:format].should eq format
+        extract_dimensions(contents).should eq [360, 240]
+      end
     end
   end
 
-  describe 'format conversion' do
-    it 'converts images to GIF' do
-      result, contents = process_image_versions('landscape.jpeg',
-        {:format => 'gif'})
+  describe 'resizing with scale "down"' do
+    let :options do
+      {:scale => :down}
+    end
 
-      result[:outputs].length.should == 1
+    describe 'landscape image' do
+      it 'resizes image, preserving aspect ratio' do
+        result, contents = process_image_version('landscape.jpeg',
+          {:width => 50, :height => 50}.merge(options))
+        extract_dimensions(contents).should eq [50, 33]
+      end
 
-      contents.length.should > 0
-      width, height = extract_dimensions(contents)
-      width.should == 360
-      height.should == 240
+      it 'resizes image, preserving aspect ratio when cropping to square' do
+        result, contents = process_image_version('landscape.jpeg',
+          {:width => 50, :height => 50, :crop => true}.merge(options))
+        extract_dimensions(contents).should eq [50, 33]
+      end
+    end
+
+    describe 'landscape image, with 270-degree rotation (EXIF orientation 6)' do
+      it 'resizes image, preserving aspect ratio' do
+        result, contents = process_image_version('landscape_rotated_270.jpeg',
+          {:width => 50, :height => 50}.merge(options))
+        extract_dimensions(contents).should eq [50, 33]
+      end
+
+      it 'resizes image, preserving aspect ratio when cropping to square' do
+        result, contents = process_image_version('landscape_rotated_270.jpeg',
+          {:width => 50, :height => 50, :crop => true}.merge(options))
+        extract_dimensions(contents).should eq [50, 33]
+      end
+    end
+
+    describe 'portrait image' do
+      it 'resizes image, preserving aspect ratio' do
+        result, contents = process_image_version('portrait.jpeg',
+          {:width => 50, :height => 50}.merge(options))
+        extract_dimensions(contents).should eq [33, 50]
+      end
+
+      it 'resizes image, preserving aspect ratio when cropping to square' do
+        result, contents = process_image_version('portrait.jpeg',
+          {:width => 50, :height => 50, :crop => true}.merge(options))
+        extract_dimensions(contents).should eq [33, 50]
+      end
+    end
+
+    describe 'portrait image, with 90-degree rotation (EXIF orientation 6)' do
+      it 'resizes image, preserving aspect ratio' do
+        result, contents = process_image_version('portrait_rotated_90.jpeg',
+          {:width => 50, :height => 50}.merge(options))
+        extract_dimensions(contents).should eq [33, 50]
+      end
+
+      it 'resizes image, preserving aspect ratio when cropping to square' do
+        result, contents = process_image_version('portrait_rotated_90.jpeg',
+          {:width => 50, :height => 50, :crop => true}.merge(options))
+        extract_dimensions(contents).should eq [33, 50]
+      end
+    end
+  end
+
+  describe 'resizing with scale "fit"' do
+    let :options do
+      {:scale => :fit}
+    end
+
+    describe 'landscape image' do
+      it 'resizes image, preserving aspect ratio' do
+        result, contents = process_image_version('landscape.jpeg',
+          {:width => 50, :height => 50}.merge(options))
+        extract_dimensions(contents).should eq [75, 50]
+      end
+
+      it 'resizes image, preserving aspect ratio when cropping to square' do
+        result, contents = process_image_version('landscape.jpeg',
+          {:width => 50, :height => 50, :crop => true}.merge(options))
+        extract_dimensions(contents).should eq [50, 50]
+      end
+    end
+
+    describe 'landscape image, with 270-degree rotation (EXIF orientation 6)' do
+      it 'resizes image, preserving aspect ratio' do
+        result, contents = process_image_version('landscape_rotated_270.jpeg',
+          {:width => 50, :height => 50}.merge(options))
+        extract_dimensions(contents).should eq [75, 50]
+      end
+
+      it 'resizes image, preserving aspect ratio when cropping to square' do
+        result, contents = process_image_version('landscape_rotated_270.jpeg',
+          {:width => 50, :height => 50, :crop => true}.merge(options))
+        extract_dimensions(contents).should eq [50, 50]
+      end
+    end
+
+    describe 'portrait image' do
+      it 'resizes image, preserving aspect ratio' do
+        result, contents = process_image_version('portrait.jpeg',
+          {:width => 50, :height => 50}.merge(options))
+        extract_dimensions(contents).should eq [50, 75]
+      end
+
+      it 'resizes image, preserving aspect ratio when cropping to square' do
+        result, contents = process_image_version('portrait.jpeg',
+          {:width => 50, :height => 50, :crop => true}.merge(options))
+        extract_dimensions(contents).should eq [50, 50]
+      end
+    end
+
+    describe 'portrait image, with 90-degree rotation (EXIF orientation 6)' do
+      it 'resizes image, preserving aspect ratio' do
+        result, contents = process_image_version('portrait_rotated_90.jpeg',
+          {:width => 50, :height => 50, :scale => :fit})
+        extract_dimensions(contents).should eq [50, 75]
+      end
+
+      it 'resizes image, preserving aspect ratio when cropping to square' do
+        result, contents = process_image_version('portrait_rotated_90.jpeg',
+          {:width => 50, :height => 50, :scale => :fit, :crop => true})
+        extract_dimensions(contents).should eq [50, 50]
+      end
     end
   end
 
